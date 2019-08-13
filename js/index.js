bc7bb9852d4e8be7b3aa6921a0b5e46f1988a3c5
LIAOJIANS <272781702@qq.com>
2019-08-14 00:48:42 +0800

    复选框完成

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 $(function () {
-
+    Dialog.init('<img src="./images/load3.gif" width="48px"/>',{
+        mask : 0,
+        addClass : 'dialog_load',
+        time : 500,
+    });
     var checks = $('.checkbox label')
     for (let i = 0; i < checks.length; i++) {
         checks[i].onclick = function () {
@@ -7,16 +11,46 @@ $(function () {
                 checks[i].className = ''
             } else {
                 checks[i].className = 'checked'
+                // console.log($(this).parent('.checkbox').next('cash').children('.ke').text())
             }
         }
     }
-    var icon_radios = $('.icon_radio')
-    for (let i = 0; i < icon_radios.length; i++) {
-        icon_radios[i].onclick = function () {
+    var radioBox = $(':radio')
+    var labs = $('.lab')
+    console.log(labs)
+    for (let i = 0; i < radioBox.length; i++) {
+        labs[i].onclick = function () {
 
         }
     }
 
+    // 缴费按钮
+    $('.btn').click(function () {
+        var checked = [] // 声明一个选中的数组对象
+        // 循环课程
+        for (let i = 0; i < checks.length; i++) {
+            if(checks[i].className === 'checked') {
+                var index = i
+                // 选中的主题
+                var kes = document.querySelectorAll('.ke')[index].innerText
+                // 选中的价格
+                var qian = document.querySelectorAll('.qian')[index].innerText
+                checked.push(
+                    { ke: kes, qian: qian }
+                )
+            }
+        }
+        console.log(checked)
+        if(checked.length === 0) {
+            Dialog.init('请选择课程', {
+                time: 1000,
+                mask: false,
+                addClass : 'toast',
+                style: 'color: #fff'
+            })
+            return;
+        }
+    })
     showC()
 })
 
@@ -41,4 +75,18 @@ function showC() {
         }
     }
 }
+
+// function payType(event) {
+//     // //
+//     // if (event.srcElement.classList[0] === 'iconfont' ) {
+//     //     event.srcElement.style.display = 'none'
+//     // } else {
+//     //     event.srcElement.children[0].style.display = 'block'
+//     // }
+//     // console.log(event)
+//     // var wx = $('.wx').is(':checked')
+//     // var zfb = $('.zfb').is(':checked')
+//    console.log()
+//
+// }
 
